eb3f5f922927e5d2b399230210fa008bffd3922b
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-09-23 13:03:51 -0400

    firebase service partial:

--- Services/FirebaseService.cs
+++ Services/FirebaseService.cs
@@ -1,19 +1,24 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Firebase.Auth;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LocalBusinessExplorer.Services
 {
     public class FirebaseService
     {
         private readonly IConfiguration _configuration;
+        private readonly FirebaseAuthClient _authClient;
 
-        public FirebaseService(IConfiguration configuration)
+        public FirebaseService(IConfiguration configuration, FirebaseAuthClient authClient)
         {
             _configuration = configuration;
+            _authClient = authClient;
+
         }
 
         public void InitializeFirebase()
@@ -26,6 +31,37 @@ namespace LocalBusinessExplorer.Services
             var appId = _configuration["Firebase:AppId"];
             var measurementId = _configuration["Firebase:MeasurementId"];
 
+        }
+
+        public async Task<string> LoginWithEmailPassword(string email, string password)
+        {
+            try
+            {
+                var userCredential = await _authClient.SignInWithEmailAndPasswordAsync(email, password);
+                //var userID = userCredential.User.Uid;
+
+                return userCredential.User.Uid;
+                //return userCredential.User?.GetIdTokenAsync().ToString();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+                return null;
+            }
+        }
+
+        public async Task<string> RegisterWithEmailPassword(string email, string password)
+        {
+            try
+            {
+                var userCredential = await _authClient.CreateUserWithEmailAndPasswordAsync(email, password);
+                return userCredential.User?.GetIdTokenAsync().ToString();
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+                return null;
+            }
         }
     }
 }
