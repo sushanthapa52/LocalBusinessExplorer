425b28c9067e8f555510703abbf9ceab5e97347f
Sushan Thapa <sushan.thapa1010@gmail.com>
2024-09-22 22:15:24 -0400

    projects settings updated

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using LocalBusinessExplorer.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace LocalBusinessExplorer
 {
@@ -15,8 +17,16 @@ namespace LocalBusinessExplorer
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            // Add appsettings.json configuration
+            var config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)  // Or use any path where your appsettings.json is stored
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            builder.Services.AddSingleton<FirebaseService>();
+
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
